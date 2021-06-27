803a8e3eb8770810530d3ee0a729b0ec0e97b491
Jose Alexandre <jose.alexandre@stoneage.com.br>
2021-06-27 18:04:59 -0300

    Join and create room functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import logo from '../assets/images/logo.svg';
 import illustration from '../assets/images/illustration.svg';
-import googleIcon from '../assets/images/google-icon.svg';
+import googleIcon from '../assets/images/google-icon-colorful.svg';
 
 import '../styles/auth.scss';
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleNewRoom() {
         if (!user) {
@@ -19,6 +22,24 @@ export function Home() {
 
         }
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+        
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = database.ref(`rooms/${roomCode}`);
+        const room = await roomRef.get();
+
+        if(!room.exists()) {
+            alert('Room does not exist');
+            return;
+        }
+
+        history.push(`rooms/${roomCode}`);
     }
 
     return (
@@ -36,8 +57,13 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input type="text" placeholder="Digite o código da sala" />
+                    <form onSubmit={handleJoinRoom}>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o código da sala" 
+                            onChange={ event => setRoomCode(event.target.value) }
+                            value={roomCode}
+                        />
                         <Button type="submit">Entrar na sala</Button>
                     </form>
                 </div>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,11 +4,32 @@ import illustration from '../assets/images/illustration.svg';
 import '../styles/auth.scss';
 
 import { Button } from '../components/Button';
-import { Link } from 'react-router-dom';
-// import { useAuth } from '../hooks/useAuth';
+import { Link, useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
-    // const { user } = useAuth();
+    const { user } = useAuth();
+    const [newRoom, setNewRoom] = useState('');
+    const history = useHistory();
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
 
     return (
         <div id="auth-page">
@@ -21,8 +42,13 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logo} alt="Letmeask" />
                     <h2>Create a new room</h2>
-                    <form>
-                        <input type="text" placeholder="Type a code for the new room" />
+                    <form onSubmit={handleCreateRoom}>
+                        <input 
+                            type="text" 
+                            placeholder="Type a code for the new room"
+                            onChange={ event => setNewRoom(event.target.value) }
+                            value={newRoom}
+                        />
                         <Button type="submit">Create room</Button>
                     </form>
                     <p>
