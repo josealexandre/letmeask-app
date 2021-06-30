96b03cdf9f109ad37c5e9a6e9b8f135323f75005
Jose Alexandre <jose.alexandre@stoneage.com.br>
2021-06-30 11:30:05 -0300

    Save new question in firebase and show user info when logged in

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import logo from '../assets/images/logo.svg';
@@ -8,6 +8,8 @@ import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 
 import '../styles/room.scss';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 type RoomParam = {
     id: string;
@@ -15,18 +17,44 @@ type RoomParam = {
 
 export function Room() {
     const params = useParams<RoomParam>();
+    const roomId = params.id;
+
+    const [newQuestion, setNewQuestion] = useState('');
+    const { user } = useAuth();    
     
-    function handleSubmitQuestion(event:FormEvent) {
+    async function handleSubmitQuestion(event:FormEvent) {
         event.preventDefault();
+
+        if (newQuestion === '') {
+            return;
+        }
+
+        if (!user) {
+            throw new Error('You must be logged in.');
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar: user.avatar
+            },
+            isHighlighted: false,
+            isAnswered: false,
+        }
+
+        await database.ref(`rooms/${roomId}/questions`).push(question)
+        setNewQuestion('');
     }
 
     return (
         <div id="room-page">
+            {console.log(newQuestion)}
             <header>
                 <div className="content">
                     <img src={logo} alt="Application's logotype - Let me ask" />
                     <div>
-                        <RoomCode code={params.id} />
+                        <RoomCode code={roomId} />
                     </div>
                 </div>
             </header>
@@ -36,12 +64,23 @@ export function Room() {
                     <span>4 questions</span>
                 </div>
                 <form onSubmit={handleSubmitQuestion}>
-                    <textarea placeholder="What do you have in mind?" />
+                    <textarea 
+                        placeholder="What do you have in mind?" 
+                        onChange={ (event) => setNewQuestion(event.target.value) }
+                        value = {newQuestion}
+                    />
                     <div className="form-footer">
-                        <span>
-                            <button>Sign in</button> to send a question
-                        </span>
-                        <Button type="submit">Send question</Button>
+                        { user ? (
+                            <div className="user-info">
+                                <img src={user.avatar} alt={user.name} />
+                                <span>{user.name}</span>
+                            </div>
+                        ) : (
+                            <span>
+                                <button>Sign in</button> to send a question
+                            </span>
+                        )}
+                        <Button type="submit" disabled={!user}>Send question</Button>
                     </div>
                 </form>
                 <div className="room-questions">
