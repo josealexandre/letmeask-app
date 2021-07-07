d4faabc18988555fb3802b1fb2bb7089918cfa33
Jose Alexandre <jose.alexandre@stoneage.com.br>
2021-07-06 23:30:24 -0300

    Mark question as answered or highlighted

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,7 @@
 import { ReactNode } from 'react';
+
+import classNames from 'classnames';
+
 import './styles.scss';
 
 type QuestionProps = {
@@ -7,19 +10,26 @@ type QuestionProps = {
         avatar: string;
         name: string;
     }
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
     children?: ReactNode;
 }
 
-export function Question({content, author, children}: QuestionProps) {
+export function Question({content, author, isAnswered = false, isHighlighted = false, children}: QuestionProps) {
     return (
-        <div className="question">            
+        <div className={classNames(
+            'question', {
+                answered: isAnswered,
+                highlighted: isHighlighted && !isAnswered,
+            }
+        )}>
             <p>{content}</p>
             <footer>
                 <div className="user-info">
                     <img src={author.avatar} alt={author.name} />
                     <span>{author.name}</span>
                 </div>
-                <div>
+                <div className="question-actions">
                     {children}
                 </div>
             </footer>
--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -6,6 +6,7 @@ import { useRoom } from '../../hooks/useRoom';
 // import { database } from '../../services/firebase';
 
 import logoImg from '../../assets/images/logo.svg';
+import checkImg from '../../assets/images/check.svg';
 import deleteImg from '../../assets/images/delete.svg';
 // import emptyQuestionsImg from '../../assets/images/empty-questions.svg';
 
@@ -36,6 +37,19 @@ export function AdminRoom() {
         }
     }
 
+    async function handleMarkAsAnsweredQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+            isHighlighted: false,
+        });
+    }
+
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true,
+        });
+    }
+
     async function handleDeleteQuestion(questionId: string) {
         if (window.confirm('Are you sure you want to delete this question?')) {
             await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
@@ -60,8 +74,26 @@ export function AdminRoom() {
                 </div>                
                 <div className="room-questions">
                     { questions.map(question => (
-                        <Question key={question.id} author={question.author} content={question.content}>
-                            <button className="delete-button">
+                        <Question 
+                            key={question.id}
+                            author={question.author}
+                            content={question.content}
+                            isAnswered={question.isAnswered}
+                            isHighlighted={question.isHighlighted}
+                        >
+                            {!question.isAnswered && 
+                                <>
+                                    <button className="check-button" onClick={() => handleMarkAsAnsweredQuestion(question.id)}>
+                                        <img src={checkImg} alt="Check question as answered" />
+                                    </button>
+                                    <button className="highlight-button" onClick={() => handleHighlightQuestion(question.id)}>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                        </svg>
+                                    </button>
+                                </>
+                            }                        
+                            <button>
                                 <img src={deleteImg} alt="Delete question" onClick={() => handleDeleteQuestion(question.id)} />
                             </button>
                         </Question>
